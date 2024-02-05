d67e3f8331277dc7aec6f6f1059645f159ca558c
Ian Whitfield <ianwhitfieldm@gmail.com>
2024-02-04 21:42:11 -0800

    screen and input works

--- src/pager/display_manager.cpp
+++ src/pager/display_manager.cpp
@@ -1,22 +1,86 @@
-#include "display_manager.h"
-
+#include <FunctionalInterrupt.h>
 #include <U8g2lib.h>
 
+#include <array>
 #include <functional>
 
 #include "DebugConfiguration.h"
+#include "display_manager.h"
 
 namespace pager
 {
 
 constexpr BaseType_t kCpuCore = 0;
-constexpr UBaseType_t kThreadPriority = 1;
+constexpr UBaseType_t kThreadPriority = 0;
 
+namespace
+{
+/*
+ * Bridge from raw function pointer to std::function for xTaskCreate.
+ */
 void startThread(void *param)
 {
     auto fn = *static_cast<std::function<void()> *>(param);
     fn();
     vTaskDelete(NULL);
+}
+}  // namespace
+
+void DisplayManager::init()
+{
+    LOG_INFO("[Pager DisplayManager] Start init.\n");
+    initInputs();
+    initScreen();
+    xTaskCreatePinnedToCore(&startThread, "display_loop", 30000, &thread_fn,
+                            kThreadPriority, &threadHandle, kCpuCore);
+
+    LOG_INFO("[Pager DisplayManager] End init.\n");
+}
+
+void DisplayManager::initInputs()
+{
+    auto setupButton = [&](int pin, int id) {
+        pinMode(pin, INPUT_PULLUP);
+        interrupts[id] = std::bind(&DisplayManager::onDown, this, id);
+        attachInterrupt(pin, interrupts[id], FALLING);
+    };
+
+    enum ButtonPin {
+        RED = 33,
+        GREEN = 34,
+        UP = 26,
+        DOWN = 48,
+        LEFT = 47,
+        RIGHT = 20
+    };
+    setupButton(ButtonPin::GREEN, Button::GREEN);
+    setupButton(ButtonPin::RED, Button::RED);
+    setupButton(ButtonPin::UP, Button::UP);
+    setupButton(ButtonPin::DOWN, Button::DOWN);
+    setupButton(ButtonPin::LEFT, Button::LEFT);
+    setupButton(ButtonPin::RIGHT, Button::RIGHT);
+}
+
+/*
+ * Caution! Interrupt handler. Must return quickly.
+ */
+void DisplayManager::onDown(int button)
+{
+    const int debounceMillis = 100;
+    unsigned long now = millis();
+    unsigned long last = button_debounce[button];
+    if ((now - last) < debounceMillis) {
+        return;
+    }
+    button_debounce[button] = now;
+    button_state[button] = true;
+    queued_input = button;
+}
+
+void DisplayManager::onUp(int button)
+{
+    // TODO: Bounce?
+    button_state[button] = false;
 }
 
 void DisplayManager::initScreen()
@@ -24,29 +88,35 @@ void DisplayManager::initScreen()
     u8g2.begin();
     u8g2.clearBuffer();
     u8g2.sendBuffer();
-
-    LOG_INFO("PAGER InitDisplay\n");
-    xTaskCreatePinnedToCore(&startThread, "display_loop", 30000, &threadFn,
-                            kThreadPriority, &threadHandle, kCpuCore);
 }
 
 void DisplayManager::thread()
 {
     for (;;) {
-        // TODO: Draw current top window
-        // Send input events
-        // Garbage collect closed windows
-        u8g2.clearBuffer();
-        for (auto &window : windows) {
-            window->repaint(u8g2);
+        if (windows.empty()) {
+            LOG_WARN("[Pager DisplayManager] No windows\n");
+            continue;
         }
+        auto &window = windows.top();
+        // Send input events
+        if (queued_input != Button::NONE) {
+            window->onEvent(queued_input);
+            queued_input = Button::NONE;
+        }
+
+        // Draw current top window
+        u8g2.clearBuffer();
+        window->repaint(u8g2);
         u8g2.sendBuffer();
+
+        // Garbage collect closed windows.
+        // TODO
     }
 }
 
 void DisplayManager::addWindow(int id, std::unique_ptr<Window> window)
 {
-    windows.push_back(std::move(window));
+    windows.push(std::move(window));
 }
 
 }  // namespace pager
